f69ab4d5dd453c63edc8789046f7b4bd5dafb7dc
Arne Müller <a.mueller@eckelmann.de>
2021-01-13 17:25:41 +0100

    Initiale Datenbank

--- infrastructure/models.py
+++ infrastructure/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+class Offer(models.Model):
+    name = models.CharField(max_length=60)
+    species = models.CharField(max_length=60)
+    breed = models.CharField(max_length=60)
+    description = models.CharField(max_length=60)
+    date_published = models.DateField()
+
+    def __str__(self):
+        return self.name
+
+class User(models.Model):
+    name = models.CharField(max_length=60)
+    date_created = models.DateField()
+    offers = models.ForeignKey(Offer, on_delete=models.DO_NOTHING)
+
+    def __str__(self):
+        return self.name
