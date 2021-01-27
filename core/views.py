e30dd12c13238fbe340fadc9ee3febb610d6ae40
KlausFu <klaus.fuhrmeister@gmx.de>
2021-01-27 11:33:46 +0100

    get single user

--- core/views.py
+++ core/views.py
@@ -1,27 +1,57 @@
 from django.contrib.auth.models import User, Group
-from rest_framework import viewsets, permissions
+from rest_framework import viewsets, permissions, renderers
+from rest_framework.decorators import action
+from rest_framework.response import Response
 
 from .permissions import IsOwnerOrReadOnly
 
 from .models import Offer, Favorites
-from .serializers import UserSerializer, GroupSerializer, OfferSerializer, FavoritesSerializer
+from .serializers import UserSerializer, OfferSerializer, FavoritesSerializer
 
 class UserViewSet(viewsets.ModelViewSet):
     queryset = User.objects.all().order_by('-date_joined')
     serializer_class = UserSerializer
-    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    permission_classes = [permissions.IsAdminUser]
 
-class GroupViewSet(viewsets.ModelViewSet):
-    queryset = Group.objects.all()
-    serializer_class = GroupSerializer
-    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    @action(detail=True, renderer_classes=[renderers.StaticHTMLRenderer])
+    def exists(self, request, *args, **kwargs):
+        return Response(self.queryset.filter(pk=self.get_object().pk).exists())
+
+
+class AuthViewSet(viewsets.ModelViewSet):
+    # queryset = User.objects.all()
+
+    serializer_class = UserSerializer
+    permission_classes = [IsOwnerOrReadOnly]
+
+    def get_queryset(self):
+
+        try:
+            user_id = self.request.query_params.get('id')
+            queryset = User.objects.filter(id=user_id)
+
+        except User.DoesNotExist:
+            # todo
+            print('err')
+            queryset = User.objects.all()
+
+        # return token
+        return queryset
+
+    # def exists(self, request, *args, **kwargs):
+    #    return Response(self.get_queryset())
+
 
 class OfferViewSet(viewsets.ModelViewSet):
     queryset = Offer.objects.all()
     serializer_class = OfferSerializer
-    permission_classes = [permissions.IsAuthenticatedOrReadOnly]#, IsOwnerOrReadOnly]
+    permission_classes = [IsAdminOrOwnerOrReadOnly]
+
+    @action(detail=True, renderer_classes=[renderers.StaticHTMLRenderer])
+    def exists(self, request, *args, **kwargs):
+        return Response(self.queryset.filter(pk=self.get_object().pk).exists())
 
 class FavoritesViewSet(viewsets.ModelViewSet):
     queryset = Favorites.objects.all()
     serializer_class = FavoritesSerializer
-    permission_classes = [permissions.IsAuthenticatedOrReadOnly]#, IsOwnerOrReadOnly]
+    permission_classes = [IsAdminOrOwnerOrReadOnly]
