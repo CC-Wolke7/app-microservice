9679b11f057f1f10861a90b7b99bfa156096d3f6
Nik Sauer <nik.sauer@me.com>
2021-02-06 18:30:09 +0100

    Remove MIME type extension breaking DRF browser

--- app_microservice/app_microservice/settings.py
+++ app_microservice/app_microservice/settings.py
@@ -9,12 +9,10 @@ https://docs.djangoproject.com/en/3.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
-import mimetypes
+
 from pathlib import Path
 
 from .helper import getenv
-
-mimetypes.add_type("text/plain", ".js", True)
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
