8248d9707084f22813cb5b3b5b10edc94e865ed8
Nik Sauer <nik.sauer@me.com>
2021-02-06 18:40:51 +0100

    Reformat and lint all files

--- app_microservice/core/views.py
+++ app_microservice/core/views.py
@@ -1,24 +1,20 @@
-import time
-
 from google.auth.transport import requests
 from google.cloud import storage
 from google.oauth2 import id_token
-from rest_framework_simplejwt.tokens import RefreshToken
 
 from rest_framework import permissions, status, viewsets
 from rest_framework.decorators import action
 from rest_framework.response import Response
 from rest_framework.views import APIView
+from rest_framework_simplejwt.tokens import RefreshToken
 
 from .models import Favorites, Offer, WSUser
-from .permissions import IsAdminOrOwner, IsAdminOrOwnerOrReadOnly
+from .permissions import IsAdminOrOwner, IsAdminOrOwnerOrReadOnly  # noqa
 from .serializers import FavoritesSerializer, OfferSerializer, WSUserSerializer
 
 
 class Images(APIView):
-
     def get(self, request, format=None):
-
         try:
             storage_client = storage.Client()
             bucket = storage_client.bucket('wolkesiebenbucket')
@@ -26,11 +22,10 @@ class Images(APIView):
             image = blob.download_as_text()
 
             return Response(image, status=status.HTTP_200_OK)
-        except:
+        except:  # noqa
             return Response(status=status.HTTP_500_INTERNAL_SERVER_ERROR)
 
     def post(self, request, format=None):
-
         try:
             storage_client = storage.Client()
             bucket = storage_client.bucket('wolkesiebenbucket')
@@ -38,8 +33,9 @@ class Images(APIView):
             blob.upload_from_string(request.data['image'])
 
             return Response(status=status.HTTP_200_OK)
-        except:
+        except:  # noqa
             return Response(status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+
 
 class WSUserViewSet(viewsets.ModelViewSet):
     queryset = WSUser.objects.all().order_by('-date_joined')
@@ -51,8 +47,12 @@ class WSUserViewSet(viewsets.ModelViewSet):
 
         google_token = request.META['HTTP_AUTHORIZATION'][7:]
 
-        #try:
-        idinfo = id_token.verify_oauth2_token(google_token, requests.Request(), '882517722597-3p6j1koj84oa27kv4bc9t58egianqf3e.apps.googleusercontent.com')
+        # try:
+        idinfo = id_token.verify_oauth2_token(
+            google_token,
+            requests.Request(),
+            '882517722597-3p6j1koj84oa27kv4bc9t58egianqf3e.apps.googleusercontent.com'  # noqa
+        )
         userid = idinfo['sub']
 
         google_user = self.queryset.filter(
@@ -69,20 +69,21 @@ class WSUserViewSet(viewsets.ModelViewSet):
             )
 
         # uuid = generare_uuid() TODO
-        refresh = RefreshToken.for_user(WSUser.objects.filter(externalId=userid)[0])
-        token = {
-            'refresh': str(refresh),
-            'access': str(refresh.access_token)
-        }
+        refresh = RefreshToken.for_user(
+            WSUser.objects.filter(externalId=userid)[0]
+        )
+        token = {'refresh': str(refresh), 'access': str(refresh.access_token)}
 
         return Response(token)
-        #except ValueError:
+        # except ValueError:
         #    return Response(status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+
 
 class OfferViewSet(viewsets.ModelViewSet):
     queryset = Offer.objects.all()
     serializer_class = OfferSerializer
     permission_classes = [permissions.AllowAny]
+
 
 class FavoritesViewSet(viewsets.ModelViewSet):
     queryset = Favorites.objects.all()
