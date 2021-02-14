e425e9be44a0c857784b80697bb676b47d058fc9
Nik Sauer <nik.sauer@me.com>
2021-02-14 18:41:55 +0100

    Enable management of all models through admin panel

--- app_microservice/core/admin.py
+++ app_microservice/core/admin.py
@@ -1,7 +1,25 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 
-from .models import User
+from .choices import Breed
+from .models import Favorite, Offer, OfferImage, Subscription, User
+
+
+class FavoriteInline(admin.TabularInline):
+    model = Favorite
+
+    fields = ["offer"]
+
+    extra = 0
+
+
+class SubscriptionInline(admin.TabularInline):
+    model = Subscription
+
+    fields = ["breed"]
+
+    extra = 0
+    max_num = len(Breed.choices)
 
 
 class UserAdmin(UserAdmin):
@@ -22,9 +40,18 @@ class UserAdmin(UserAdmin):
     list_filter = ["is_staff"]
 
     fieldsets = [
-        [None, {
-            "fields": ["name", "email", "uuid", "password"],
-        }],
+        [
+            None, {
+                "fields": [
+                    "name",
+                    "email",
+                    "uuid",
+                    "password",
+                    "description",
+                    "profile_image_name",
+                ],
+            }
+        ],
         [
             "Django Permissions", {
                 "classes": ["collapse"],
@@ -35,5 +62,25 @@ class UserAdmin(UserAdmin):
         ],
     ]
 
+    inlines = [FavoriteInline, SubscriptionInline]
+
+
+class OfferImageInline(admin.TabularInline):
+    model = OfferImage
+
+    fields = ["name"]
+
+
+class OfferAdmin(admin.ModelAdmin):
+    fields = [
+        "uuid", "date_published", "published_by", "name", "age", "species",
+        "breed", "sex", "sterile", "description", "location"
+    ]
+    readonly_fields = ["uuid", "date_published", "published_by"]
+    list_display = ["name", "species", "breed", "published_by", "uuid"]
+
+    inlines = [OfferImageInline]
+
 
 admin.site.register(User, UserAdmin)
+admin.site.register(Offer, OfferAdmin)
