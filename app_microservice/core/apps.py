9d2b54c8388b21d8a26672f2c578e905542616ed
Nik Sauer <nik.sauer@me.com>
2021-02-15 15:11:11 +0100

    Refactor offer created notification
    - Move publishing logic to `publisher.py`
    - Use `post_save` hook to trigger notification
    
    Closes #3

--- app_microservice/core/apps.py
+++ app_microservice/core/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 from django.contrib import admin
-from django.db.models.signals import pre_delete
+from django.db.models.signals import post_save, pre_delete
 
 
 class CoreConfig(AppConfig):
@@ -16,10 +16,18 @@ class CoreConfig(AppConfig):
 
     def setup_signals(self):
         from .models import Offer
-        from .signals import delete_offer_images
+        from .signals import (
+            delete_offer_images, send_offer_created_notification
+        )
 
         pre_delete.connect(
             delete_offer_images,
             sender=Offer,
             dispatch_uid='core.signals.delete_offer_images'
+        )
+
+        post_save.connect(
+            send_offer_created_notification,
+            sender=Offer,
+            dispatch_uid='core.signals.send_offer_created_notification'
         )
